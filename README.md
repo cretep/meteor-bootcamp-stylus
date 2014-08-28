12e2129107a90850928a64561c83ad93e2184822
Pete C <cretep@users.noreply.github.com>
2014-08-28 12:31:59 +0100

    info on hack and testing

--- README.md
+++ README.md
@@ -1,17 +1,23 @@
+<span style="color: red">
+Requires dirty hack for Meteor 0.9 :(
+=======================
+```
+git clone https://github.com/cretep/meteor-bootcamp-stylus "packages/pac:bootcamp-stylus"
+```
+</span>
+
 Bootstrap 3.2.0
 ===============
 
-[Bootstrap 3.2.0](https://github.com/twbs/bootstrap) to [Stylus 0.42.0](http://learnboost.github.com/stylus/). 
+[Bootstrap 3.2.0](https://github.com/twbs/bootstrap) in [Stylus 0.42.0](http://learnboost.github.com/stylus/) by [Acquisio](https://github.com/Acquisio/bootstrap-stylus).
 
-Converted by [Acquisio](https://github.com/Acquisio/bootstrap-stylus).
- 
 
 Quick Start
 ===========
 
 Create a bootstrap.styl file in your project and add:
 ```
-@import "packages/pac:bootstrap-stylus/bootstrap.styl"
+@import "/packages/pac:bootstrap-stylus/bootstrap.styl"
 ```
 
 
--- package.js
+++ package.js
@@ -1,6 +1,7 @@
 Package.describe({
   summary: "Bootstrap Stylus",
-  version: "3.2.0"
+  version: "0.0.3",
+  git: "https://github.com/cretep/meteor-bootcamp-stylus.git"
 });
 
 Package.onUse(function(api) {
@@ -27,5 +28,54 @@ Package.onUse(function(api) {
     'js/tab.js',
     'js/affix.js'
     ], 'client');
+
+  api.addFiles([
+    "import/variables.import.styl",
+    "import/mixins.import.styl",
+
+    "import/normalize.import.styl",
+    "import/print.import.styl",
+    "import/glyphicons.import.styl",
+
+    "import/scaffolding.import.styl",
+    "import/utilities.import.styl",
+    "import/type.import.styl",
+    "import/code.import.styl",
+    "import/grid.import.styl",
+    "import/tables.import.styl",
+    "import/forms.import.styl",
+    "import/buttons.import.styl",
+
+    "import/component-animations.import.styl",
+    "import/dropdowns.import.styl",
+    "import/button-groups.import.styl",
+    "import/input-groups.import.styl",
+    "import/navs.import.styl",
+    "import/navbar.import.styl",
+    "import/breadcrumbs.import.styl",
+    "import/pagination.import.styl",
+    "import/pager.import.styl",
+    "import/labels.import.styl",
+    "import/badges.import.styl",
+    "import/jumbotron.import.styl",
+    "import/thumbnails.import.styl",
+    "import/alerts.import.styl",
+    "import/progress-bars.import.styl",
+    "import/media.import.styl",
+    "import/list-group.import.styl",
+    "import/panels.import.styl",
+    "import/responsive-embed.import.styl",
+    "import/wells.import.styl",
+    "import/close.import.styl",
+
+    "import/modals.import.styl",
+    "import/tooltip.import.styl",
+    "import/popovers.import.styl",
+    "import/carousel.import.styl",
+
+    "import/responsive-utilities.import.styl",
+    "import/bootstrap.import.styl"
+    ], 'client');
+
 
 });
